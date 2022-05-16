96c982afe5fc29c68dfb0c229532bc2f1680580a
John Schellinger <schellingerj1@gmail.com>
2022-05-15 23:32:26 -0400

    Deploy

--- handler.js
+++ handler.js
@@ -1,140 +1,140 @@
-'use strict';
-const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require('@aws-sdk/client-apigatewaymanagementapi');
-const { DynamoDBClient, PutItemCommand, DeleteItemCommand, ScanCommand } = require('@aws-sdk/client-dynamodb');
+const {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} = require('@aws-sdk/client-apigatewaymanagementapi');
+const {
+  DynamoDBClient,
+  PutItemCommand,
+  DeleteItemCommand,
+  ScanCommand,
+} = require('@aws-sdk/client-dynamodb');
 
-const CONNECTION_DB_TABLE = process.env.CONNECTION_DB_TABLE;
+const connectionTable = process.env.CONNECTION_TABLE;
 
-const successfulResponse = {
-  statusCode: 200,
-  body: 'Success',
+const formatJsonResponse = (body) => {
+  return {
+    statusCode: 200,
+    body: body || {},
+  };
 };
 
-const failedResponse = (statusCode, error) => ({
-  statusCode,
-  body: error,
-});
+const formatJsonError = (statusCode, err) => {
+  return {
+    statusCode: statusCode,
+    body: { err },
+  };
+};
 
-module.exports.connectHandler = (event, context, callback) => {
+module.exports.connectHandler = async (event, context, callback) => {
   addConnection(event.requestContext.connectionId)
     .then(() => {
-      callback(null, successfulResponse);
+      callback(formatJsonResponse(), null);
     })
     .catch((err) => {
-      callback(failedResponse(500, JSON.stringify(err)));
+      callback(null, formatJsonError(500, err));
     });
 };
 
-module.exports.disconnectHandler = (event, context, callback) => {
+module.exports.disconnectHandler = async (event, context, callback) => {
   deleteConnection(event.requestContext.connectionId)
     .then(() => {
-      callback(null, successfulResponse);
+      callback(formatJsonResponse(), null);
     })
     .catch((err) => {
-      console.error(err);
-      callback(failedResponse(500, JSON.stringify(err)));
+      callback(null, formatJsonError(500, err));
     });
 };
 
-module.exports.defaultHandler = (event, context, callback) => {
-  callback(null, failedResponse(404, 'No event found'));
+module.exports.defaultHandler = async (event, context, callback) => {
+  console.warn(`Default handler invoked:\n${event}`);
+  callback(null, formatJsonError(404, 'No handler found'));
 };
 
-module.exports.sendMessageHandler = (event, context, callback) => {
-  sendMessageToAllConnected(event)
-    .then(() => {
-      callback(null, successfulResponse);
+module.exports.sendMessageHandler = async (event, context, callback) => {
+  getAllConnections()
+    .then((connectionData) => {
+      const postData = event.body.data;
+      connectionData.Items.map(
+        (connectionDataItem) => connectionDataItem.connectionId.S
+      ).forEach((connectionId) => {
+        send(event, connectionId, postData);
+      });
+      console.info('Broadcasted a message', { data });
+      callback(formatJsonResponse(), null);
     })
     .catch((err) => {
-      console.error(err);
-      callback(failedResponse(500, JSON.stringify(err)));
+      console.error('Failed to broadcast message', err);
+      callback(null, formatJsonError(500, err));
     });
 };
 
-const sendMessageToAllConnected = (event) => {
-  return getAllConnections().then((connectionData) => {
-    return connectionData.Items.map((connectionId) => {
-      return send(event, connectionId.connectionId);
-    });
-  });
-};
-
-const getAllConnections = async () => {
-  const params = {
-    TableName: CONNECTION_DB_TABLE,
-    ProjectionExpression: 'connectionId',
-  };
-  const client = new DynamoDBClient({ region: process.env.REGION });
-  const command = new ScanCommand(params);
-  return await client.send(command);
-};
-
-const send = async (event, connectionId) => {
-  const body = JSON.parse(event.body);
-  let postData = body.data;
-  console.log('Sending.....');
-
-  if (typeof postData === 'object') {
-    console.log('It was an object');
-    postData = JSON.stringify(postData);
-  } else if (typeof postData === 'string') {
-    console.log('It was a string');
-  }
-
-  const endpoint = event.requestContext.domainName + '/' + event.requestContext.stage;
-  const client = ApiGatewayManagementApiClient({
+const send = (event, connectionId, postData) => {
+  const endpoint =
+    event.requestContext.domainName + '/' + event.requestContext.stage;
+  const client = new ApiGatewayManagementApiClient({
     region: process.env.REGION,
     endpoint: endpoint,
   });
 
-  const params = {
+  const command = new PostToConnectionCommand({
     ConnectionId: connectionId,
     Data: postData,
-  };
-  const command = new PostToConnectionCommand(params);
-  const data = await client.send(command);
-  console.info('Broadcasted a message', { params }, { data });
+  });
+
+  client.send(command);
 };
 
-const addConnection = async (connectionId) => {
-  const params = {
-    TableName: CONNECTION_DB_TABLE,
+const getAllConnections = () => {
+  const client = new DynamoDBClient({ region: process.env.REGION });
+  const command = new ScanCommand({
+    TableName: connectionTable,
+    ProjectionExpression: 'connectionId',
+  });
+  return client.send(command);
+};
+
+const addConnection = (connectionId) => {
+  const command = new PutItemCommand({
+    TableName: connectionTable,
     Item: {
-      connectionId: connectionId,
+      connectionId: {
+        S: connectionId,
+      },
     },
-  };
+  });
 
   const client = new DynamoDBClient({ region: process.env.REGION });
-  const command = new PutItemCommand(params);
-
-  // async/await.
-  try {
-    const data = await client.send(command);
-    console.info('New connection', { data });
-  } catch (error) {
-    console.error('Failed to add connection', { error });
-    throw error;
-  }
+  return client
+    .send(command)
+    .then((data) => {
+      console.info('New connection', { data });
+      return data;
+    })
+    .catch((error) => {
+      console.error('Failed to add connection', { error });
+    });
 };
 
-const deleteConnection = async (connectionId) => {
-  const params = {
-    TableName: CONNECTION_DB_TABLE,
+const deleteConnection = (connectionId) => {
+  const command = new DeleteItemCommand({
+    TableName: connectionTable,
     Key: {
-      connectionId: connectionId,
+      connectionId: {
+        S: connectionId,
+      },
     },
-  };
+  });
 
   const client = new DynamoDBClient({ region: process.env.REGION });
-  const command = new DeleteItemCommand(params);
-
-  // async/await.
-  try {
-    const data = await client.send(command);
-    console.info('Deleted connection', { data });
-  } catch (error) {
-    console.error('Failed to delete connection', { error });
-    throw error;
-  }
+  return client
+    .send(command)
+    .then((data) => {
+      console.info('Deleted connection', { data });
+      return data;
+    })
+    .catch((error) => {
+      console.error('Failed to delete connection', { error });
+    });
 };
 
 // TODO
